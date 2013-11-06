e0d9099f291137798634546c85bba0a62d7bda87
Chris Osterwood <osterwood@gmail.com>
2013-11-05 22:32:28 -0500

    Update kicad.rb
    Builds successfully on OS 10.8.5, kicad source as of 2013-11-05

--- kicad.rb
+++ kicad.rb
@@ -3,7 +3,16 @@ require 'formula'
 class KicadLibrary < Formula
   homepage 'https://code.launchpad.net/~kicad-lib-committers/kicad/library'
   head 'https://code.launchpad.net/~kicad-lib-committers/kicad/library', :using => :bzr
-  def initialize; super 'kicad-library'; end
+
+  def  patches
+    [
+      "https://gist.github.com/shaneburrell/5415623/raw/0d79ff29cdcc2b01a2366ec3a52ce1f2e8ef0e0f/gistfile1.txt"
+    ]
+  end
+
+  def initialize
+    super 'kicad-library'
+  end
 end
 
 class Kicad < Formula
@@ -14,18 +23,16 @@ class Kicad < Formula
   depends_on 'cmake' => :build
   depends_on :x11
   depends_on 'Wxmac'
-  depends_on 'GLEW'
+  depends_on 'glew'
+  depends_on 'cairo'
+  depends_on 'doxygen'
 
   def patches
     [
-    # fixes wx-config not requiring aui module
-    #"https://gist.github.com/raw/4602653/0e4397884062c8fc44a9627e78fb4d2af20eed5b/gistfile1.txt",
-    # enable retina display for OSX
-    #"https://gist.github.com/raw/4602849/2fe826c13992c4238a0462c03138f4c6aabd4968/gistfile1.txt",
-    #Various small patches to KICAD for OSX
-    #"https://gist.github.com/shaneburrell/5255741/raw/c34c16f4b9a5895b53dd1e1f494515652de290b1/kicad-patch.txt"
-    # Don't use bzr patch, it's from bzrtools which isn't part of homebrew's bazaar
-    #"https://gist.github.com/raw/5744451/d955cdf73968029a17f8b89f420345da40d91569/gistfile1.txt"
+      # enable retina display for OSX
+      "https://gist.github.com/raw/4602849/2fe826c13992c4238a0462c03138f4c6aabd4968/gistfile1.txt",
+      # Don't use bzr patch, it's from bzrtools which isn't part of homebrew's bazaar
+      "https://gist.github.com/osterwood/7313647/raw/981455d7d6c4822cb86c23581fc67e9c9f8b9918/gistfile1.txt"
     ]
   end
 
@@ -41,11 +48,13 @@ class Kicad < Formula
       system "cmake", ".", *args
       system "make install"
     end
+
     args = std_cmake_args + %W[
         -DKICAD_TESTING_VERSION=ON
         -DCMAKE_CXX_FLAGS=-D__ASSERTMACROS__
       ]
 
+    system "cd ./pagelayout_editor/ && wget https://gist.github.com/osterwood/7330400/raw/99bd82ca0c25e47ceebc88dc5f2e03495d8c26da/Info.plist"
     system "cmake", ".", *args
 
     # fix the osx search path for the library components to the homebrew directory
@@ -76,3 +85,4 @@ class Kicad < Formula
     system "open #{bin}/kicad.app"
   end
 end
+
